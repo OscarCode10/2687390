0b08d449a08d4e5c92c8c03c8464d99dae6049d6
OscarCode10 <oscaortiz10@gmail.com>
2023-02-24 12:03:01 -0500

    Añadí Challenge5 y en proceso budget2

--- src/Challenge5.java
+++ src/Challenge5.java
@@ -4,16 +4,23 @@ public class Challenge5 {
     public static void main(String[] args) {
         Scanner reading = new Scanner(System.in);
         String name, surname;
-        System.out.println("Ingrese su numbre");
+        System.out.println("Ingrese su nombre");
         name = reading.nextLine();
         System.out.println("Ingrese su apellido");
         surname = reading.nextLine();
         
         int longName = name.length();
         int longSurname = surname.length();
+        String fullName = name.toUpperCase() +" "+ surname.toLowerCase();
+        String letter = name.substring(0, 2);
 
-        System.out.println("El numero de carácteres del nombre son: "+longName+" y el del apellido son: "+longSurname);
+        System.out.println("Su nombre es: "+name.toUpperCase()+" y su apellido es: "+surname.toLowerCase()+
+        ", su nombre completo es: "+fullName+"El numero de letras del nombre son: "+longName+" y el del apellido son: "+
+        longSurname+"Su apellido junto a sus dos primeras letras del nombre son: "+letter+surname);
+
 
         reading.close();
+
+
     }
 }
